a49dd82d09cd936f43a367fa9d662707f4636cdd
MarcoJHB <marcothepolo@gmail.com>
2022-08-17 09:39:45 +0200

    Add styling changes

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import styles from '../styles/BlogCard.module.css';
 
+
+
 export default function BlogPost({ title, category, coverPhoto, datePublished, slug }) {
   return (
     <div className={styles.card}>
@@ -9,7 +11,7 @@ export default function BlogPost({ title, category, coverPhoto, datePublished, s
         </div> */}
       <p className={styles.category}>{category.name}</p>
       <div className={styles.text}>
-        <Link href={'/resources/' + slug}>
+        <Link href={'/blog/' + slug}>
           <h2>{title}</h2>
         </Link>
       </div>
--- pages/blog.js
+++ pages/blog.js
@@ -2,9 +2,6 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { GraphQLClient, gql } from 'graphql-request';
 import BlogCard from '../components/BlogCard.js';
-import Link from 'next/link';
-
-const colorRange = ['#6FCF97', '#F2C94C', '#56CCF2', '#F2994A', '#2D9CDB', '#BB6BD9'];
 
 const graphcms = new GraphQLClient(
   'https://api-eu-west-2.hygraph.com/v2/cl6vsf4ud3ejc01t605uw0vdo/master'
@@ -44,7 +41,7 @@ export default function Home({ posts }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>My Portfolio</title>
+        <title>Blog | My Portfolio</title>
         <meta
           name="description"
           content="Web Development and Web Design, here is my portfolio that shows off my projects and industry insights"
@@ -53,29 +50,17 @@ export default function Home({ posts }) {
       </Head>
 
       <main className={styles.main}>
-        {posts
-          .map((post) => (
-            <BlogCard
-              title={post.title}
-              category={post.category}
-              coverPhoto={post.coverPhoto}
-              key={post.id}
-              datePublished={post.datePublished}
-              slug={post.slug}
-            />
-          ))
-          .sort((a, b) => {
-            if (a.datePublished < b.date) {
-              return 1;
-            } else {
-              return -1;
-            }
-          })
-          .slice(0, 6)}
+        {posts.map((post) => (
+          <BlogCard
+            title={post.title}
+            category={post.category}
+            coverPhoto={post.coverPhoto}
+            key={post.id}
+            datePublished={post.datePublished}
+            slug={post.slug}
+          />
+        ))}
       </main>
-      <Link href="blog">
-        <button>More Posts</button>
-      </Link>
     </div>
   );
 }
